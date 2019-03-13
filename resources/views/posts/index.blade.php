79eecc6d4ddf2585005ad5ed388d8a930745cb69
unknown <korirgeorge71@gmail.com>
2019-03-13 18:12:32 +0300

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hello this is my wabpage</title>
+</head>
+<body>
+<div class="jumbtron">
+<h1>posts</h1></div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+<div class="card">
+<div class="card-boy">
+<div class="card-tittle">
+product name
+</div>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
